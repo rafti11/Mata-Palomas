66c6614d6971bb566b8f8498dbeb5ddaaded80fd
Rafa <moducis5@gmail.com>
2016-10-20 09:35:55 +0200

    Creacion del fondo
    Aqui es cuando cree la carpeta de imagenes y la funcion
    para cargar la imagen

--- juego.py
+++ juego.py
@@ -1,18 +1,27 @@
 import sys, pygame
 from pygame.locals import *
  
-
-WIDTH = 640
-HEIGHT = 480
-
+WIDTH = 750
+HEIGHT = 500
  
+def load_image(filename, transparent=False):
+    image = pygame.image.load(filename)
+    return image
+ 
+
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Mata pajaros")
+ 
+    background_image = load_image('images/fondo.jpg')
+ 
     while True:
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
+ 
+        screen.blit(background_image, (0, 0))
+        pygame.display.flip()
     return 0
  
 if __name__ == '__main__':
