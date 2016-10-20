831a7ff8dd4a03035eee00445f3a27f9e6d6662b
Rafa <moducis5@gmail.com>
2016-10-20 09:51:29 +0200

    Añadir sprite del pajaro
    Añadimos la imagen del pajaro a nuestro juego

--- juego.py
+++ juego.py
@@ -3,7 +3,17 @@ from pygame.locals import *
  
 WIDTH = 750
 HEIGHT = 500
- 
+
+class Pajaro(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image("images/pajaro.png", True)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.centery = HEIGHT / 2
+        self.speed = [0.5, -0.5]
+
+
 def load_image(filename, transparent=False):
     image = pygame.image.load(filename)
     return image
@@ -14,6 +24,7 @@ def main():
     pygame.display.set_caption("Mata pajaros")
  
     background_image = load_image('images/fondo.jpg')
+    pajaro = Pajaro()
  
     while True:
         for eventos in pygame.event.get():
@@ -21,6 +32,7 @@ def main():
                 sys.exit(0)
  
         screen.blit(background_image, (0, 0))
+        screen.blit(pajaro.image, pajaro.rect)
         pygame.display.flip()
     return 0
  
